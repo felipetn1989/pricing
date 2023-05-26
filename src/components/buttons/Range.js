ad71d018eac33a93090586abc6023b5b9a488045
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-05-26 10:42:51 -0300

    update

--- src/components/buttons/Range.js
+++ src/components/buttons/Range.js
@@ -8,15 +8,17 @@ export default function Range({
   handleOnChange,
 }) {
   return (
-    <input
-      className={`w-[17.5rem] h-5 cursor-pointer ${styles}`}
-      type="range"
-      name={name}
-      id={name}
-      min={min}
-      max={max}
-      defaultValue={defaultValue}
-      onChange={handleOnChange}
-    />
+    <label htmlFor={name}>
+      <input
+        className={`w-[17.5rem] h-5 cursor-pointer ${styles}`}
+        type="range"
+        name={name}
+        id={name}
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        onChange={handleOnChange}
+      />
+    </label>
   );
 }
